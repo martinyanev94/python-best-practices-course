644fba455cf8c54c72624aaaef0c6fb20b311f64
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-08-14 23:27:27 -0400

    Add Lesson_24_Summary_of_Key_Patterns_in_Software_Engineering/part_1_singleton.py.py

---
+++ Lesson_24_Summary_of_Key_Patterns_in_Software_Engineering/part_1_singleton.py.py
@@ -0,0 +1,13 @@
+class Singleton:
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super(Singleton, cls).__new__(cls)
+        return cls._instance
+
+# Example usage
+singleton1 = Singleton()
+singleton2 = Singleton()
+
+print(singleton1 is singleton2)  # This will print True
