b22a3d0731f6d107b28a0acb086aa11cdc0f3be2
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-08-14 23:26:59 -0400

    Add Lesson_1_Introduction_to_Design_Principles/part_4_message_service.py

---
+++ Lesson_1_Introduction_to_Design_Principles/part_4_message_service.py
@@ -0,0 +1,21 @@
+class MessageService:
+    def __init__(self, sender):
+        self.sender = sender
+
+    def send_message(self, message: str):
+        self.sender.send(message)
+
+class EmailSender:
+    def send(self, message: str):
+        print(f"Sending email: {message}")
+
+class SMSSender:
+    def send(self, message: str):
+        print(f"Sending SMS: {message}")
+
+if __name__ == "__main__":
+    email_service = MessageService(EmailSender())
+    email_service.send_message("Hello via Email")
+
+    sms_service = MessageService(SMSSender())
+    sms_service.send_message("Hello via SMS")
