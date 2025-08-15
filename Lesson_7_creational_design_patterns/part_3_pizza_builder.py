edf1038d8bfe0ec6170646c00af5965eb86666b0
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-08-14 23:29:07 -0400

    Add Lesson_7_creational_design_patterns/part_3_pizza_builder.py

---
+++ Lesson_7_creational_design_patterns/part_3_pizza_builder.py
@@ -0,0 +1,36 @@
+class Pizza:
+    def __init__(self):
+        self.toppings = []
+
+    def add_topping(self, topping):
+        self.toppings.append(topping)
+
+    def __str__(self):
+        return f"Pizza with toppings: {', '.join(self.toppings)}"
+
+class PizzaBuilder:
+    def __init__(self):
+        self.pizza = Pizza()
+
+    def add_cheese(self):
+        self.pizza.add_topping("Cheese")
+        return self
+
+    def add_pepperoni(self):
+        self.pizza.add_topping("Pepperoni")
+        return self
+
+    def add_olives(self):
+        self.pizza.add_topping("Olives")
+        return self
+
+    def build(self):
+        return self.pizza
+
+# Client code
+builder = PizzaBuilder()
+pizza = (builder.add_cheese()
+              .add_pepperoni()
+              .add_olives()
+              .build())
+print(pizza)
